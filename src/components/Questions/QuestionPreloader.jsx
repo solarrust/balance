169b5a6a824ac1a1a0671b5b5e48b98de7c7042f
Alena Batitskaya <solar.rust@gmail.com>
2020-04-07 19:44:37 +0300

    Question Preloader moved into separated component

---
+++ src/components/Questions/QuestionPreloader.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class QuestionPreloader extends Component {
+  bgImage(imgName) {
+    const imageUrl = require(`../../img/${imgName}`);
+
+    return (
+      <img
+        className="preloader__img"
+        src={imageUrl}
+        rel="prefetch"
+        rel="preload"
+        alt="preloader photo"
+        aria-hidden="true"
+      />
+    );
+  }
+
+  render() {
+    const { gradient, image, index, sphere } = this.props;
+
+    return (
+      <div
+        className={`preloader test-card__preloader question__preloader _${index}`}
+        style={{
+          background: `linear-gradient(${gradient})`
+        }}
+      >
+        {this.bgImage(image)}
+        <div className="preloader__content">
+          <span className="preloader__text _left">balance</span>
+          <div className="preloader__logo inner-logo">
+            <div className="inner-logo__circle _half" />
+            <div className="inner-logo__circle _full">
+              <span className="preloader__text _center">your</span>
+            </div>
+          </div>
+          <span className="preloader__text _right">{sphere}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QuestionPreloader;
