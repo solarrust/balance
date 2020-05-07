1163c1ca5d7bbaadfc768b00fe3160d4b3a40677
Alena Batitskaya <solar.rust@gmail.com>
2020-05-07 12:56:33 +0300

    ·

--- src/components/Result/ShareBtns.jsx
+++ src/components/Result/ShareBtns.jsx
@@ -3,7 +3,7 @@ import { Facebook, Twitter } from "react-sharingbuttons";
 
 class ShareBtns extends Component {
   render() {
-    const local = `${window.location.protocol}://${window.location.hostname}`;
+    const local = `${window.location.protocol}//${window.location.hostname}`;
     const url = `${local}/share`;
     const shareText = "Check this site!";
 
