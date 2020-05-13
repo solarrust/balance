7514fd5b5a013e94e2624a930518f2adacb63667
Alena Batitskaya <solar.rust@gmail.com>
2020-05-13 21:20:11 +0300

    Express compression added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const compression = require("compression");
 const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
+app.use(compression());
 
 app.use(require("prerender-node"));
 
