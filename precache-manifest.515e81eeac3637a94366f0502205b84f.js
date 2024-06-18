b96477b125d7ad54f9c21c15965b0d9c62934951
Alena Batitskaia <batickaya.a@gmail.com>
2024-06-18 12:19:06 +0100

    Updates

---
+++ precache-manifest.515e81eeac3637a94366f0502205b84f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3da4796eaeabc0fa0e75ebcb9edb53f",
+    "url": "/balance/index.html"
+  },
+  {
+    "revision": "22a5043c39492a00d0fb",
+    "url": "/balance/static/css/2.39fad44b.chunk.css"
+  },
+  {
+    "revision": "ac62408ad01e378777d2",
+    "url": "/balance/static/css/main.1f998aae.chunk.css"
+  },
+  {
+    "revision": "22a5043c39492a00d0fb",
+    "url": "/balance/static/js/2.89975a3b.chunk.js"
+  },
+  {
+    "revision": "ac62408ad01e378777d2",
+    "url": "/balance/static/js/main.9fdae6f0.chunk.js"
+  },
+  {
+    "revision": "774de2c287b61f8bdf32",
+    "url": "/balance/static/js/runtime~main.c4b7f293.js"
+  },
+  {
+    "revision": "332cbcde086ff400977d2498a0949003",
+    "url": "/balance/static/media/Alena.332cbcde.png"
+  },
+  {
+    "revision": "44a19268a4091bfe0a0ec155e94ae025",
+    "url": "/balance/static/media/B-Wide-Bold.44a19268.woff2"
+  },
+  {
+    "revision": "aba11eee43be22b466f5b80f7048e4bf",
+    "url": "/balance/static/media/Danilina.aba11eee.png"
+  },
+  {
+    "revision": "67e2ee257f73f527572284daa8e70c96",
+    "url": "/balance/static/media/Elena.67e2ee25.png"
+  },
+  {
+    "revision": "cdbb3a715f8b15d3e9ac7d7944711db7",
+    "url": "/balance/static/media/Elina.cdbb3a71.png"
+  },
+  {
+    "revision": "637b18a22b7c4b782dacaa201ace48f4",
+    "url": "/balance/static/media/Natasha.637b18a2.png"
+  },
+  {
+    "revision": "12cd5d0ebcad0fa5dd1fbdc54b3b375c",
+    "url": "/balance/static/media/about-ellipse.12cd5d0e.png"
+  },
+  {
+    "revision": "2d2039acc206ba42aa48700e6b66713c",
+    "url": "/balance/static/media/arrow.2d2039ac.svg"
+  },
+  {
+    "revision": "79dce356db789a08ddd97a8c74e9a36a",
+    "url": "/balance/static/media/balance-card.79dce356.jpg"
+  },
+  {
+    "revision": "e149a87235323fcac78bd2b83ff8f40b",
+    "url": "/balance/static/media/col-icon.e149a872.svg"
+  },
+  {
+    "revision": "489a2c1df3f7797452131a0751044489",
+    "url": "/balance/static/media/fb.489a2c1d.svg"
+  },
+  {
+    "revision": "d41df432be013cff827d707d672906ec",
+    "url": "/balance/static/media/goose-bkg-tablet.d41df432.png"
+  },
+  {
+    "revision": "aaa54a70395ae7fbdcac78eebcbe42c8",
+    "url": "/balance/static/media/goose-bkg.aaa54a70.png"
+  },
+  {
+    "revision": "7eaece48f66fb068e2ec5fc96af92a8d",
+    "url": "/balance/static/media/logo.7eaece48.svg"
+  },
+  {
+    "revision": "ee368d8c885abbd5019f030fcc2a3bd5",
+    "url": "/balance/static/media/preloader_family.ee368d8c.jpg"
+  },
+  {
+    "revision": "c4195e17f08767e6cb1b12569bd5fb79",
+    "url": "/balance/static/media/preloader_finance.c4195e17.jpg"
+  },
+  {
+    "revision": "4d437d376224e86f75c25329129c8d8b",
+    "url": "/balance/static/media/preloader_friends.4d437d37.jpg"
+  },
+  {
+    "revision": "df2ea34325e5c6b81dff3135b4fad81c",
+    "url": "/balance/static/media/preloader_health.df2ea343.jpg"
+  },
+  {
+    "revision": "4a3eb155823183bcfe007045c9d31665",
+    "url": "/balance/static/media/preloader_rel.4a3eb155.jpg"
+  },
+  {
+    "revision": "e3c5fba5b6bf76fc5bbe5f62ab847ff9",
+    "url": "/balance/static/media/preloader_relax.e3c5fba5.jpg"
+  },
+  {
+    "revision": "36db02550c4dc61da6ab11a3844016eb",
+    "url": "/balance/static/media/preloader_self.36db0255.jpg"
+  },
+  {
+    "revision": "6947dd406536fb14b6bba456b1a38877",
+    "url": "/balance/static/media/preloader_work.6947dd40.jpg"
+  },
+  {
+    "revision": "80000079490045a1d8875b109dbe82b2",
+    "url": "/balance/static/media/row-icon.80000079.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/balance/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/balance/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/balance/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/balance/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b99a3dabb810e482ae83338310aef36d",
+    "url": "/balance/static/media/twitter.b99a3dab.svg"
+  }
+]);
